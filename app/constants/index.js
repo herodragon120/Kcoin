306b148fe3b6ef6884f63088fdbc1930bce254da
MiM <phuctlq113@gmail.com>
2018-01-03 14:25:46 +0700

    demo database

--- app/constants/index.js
+++
@@ -1,5 +0,0 @@
-const constants =  {
-    TOGGLE_LOGIN: "TOGGLE_LOGIN",
-};
-
-export default constants;
--- app/reducers/index.js
+++
@@ -1,8 +0,0 @@
-const redux = require('redux');
-const isLogin=require("./isLogin");
-
-var reducer=redux.combineReducers({
-    isLogin
-});
-
-module.exports = reducer;
